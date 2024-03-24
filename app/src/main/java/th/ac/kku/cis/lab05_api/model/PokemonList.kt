3a8da16db295766ea55eccf6e1f2e99e06747dd2
Panuwit Passatho <76519770+papanuwit@users.noreply.github.com>
2024-03-24 22:45:45 +0700

    add api

---
+++ app/src/main/java/th/ac/kku/cis/lab05_api/model/PokemonList.kt
@@ -0,0 +1,13 @@
+package th.ac.kku.cis.lab05_api.model
+
+data class PokemonList(
+    val count: Int,
+    val next: String?,
+    val previous: String?,
+    val results: List<Pokemon>
+)
+
+data class Pokemon(
+    var name: String,
+    val url: String
+)
